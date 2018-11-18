7ec89fb409c2720e2a33bf5bd52b04c384b29386
Jack Ng <jhwngpro@gmail.com>
2018-11-18 01:46:09 -0500

    Changed MySQL connection code to use non-persistent connections
    * Added mysql_select_db() call in Connections/promusic.php (for user_admin.php)

--- Connections/promusic.php
+++ Connections/promusic.php
@@ -6,7 +6,19 @@ $hostname_promusic = "localhost";
 $database_promusic = "xxx";
 $username_promusic = "yyy";
 $password_promusic = "zzz";
-$promusic = mysql_pconnect($hostname_promusic, $username_promusic, $password_promusic) or trigger_error(mysql_error(),E_USER_ERROR); 
+
+//$promusic = mysql_pconnect($hostname_promusic, $username_promusic, $password_promusic) or trigger_error(mysql_error(),E_USER_ERROR);
+
+//xxxjng - Use non-persistent connection to avoid potential "weird" issues
+//         with transactions (for instance, since the transactions in this
+//         PHP app are NOT explicitly rolled back if a page hits an error,
+//         it means the executed insert/update/delete could be inadvertently
+//         committed if subsequently another page issues another
+//         "START TRANSACTION" --which is expected behavior for MySQL).
+//         The 4th parameter of mysql_connect() is for 'new_link' and
+//         'true' means always get a new link.
+$promusic = mysql_connect($hostname_promusic, $username_promusic, $password_promusic, true) or trigger_error(mysql_error(),E_USER_ERROR);
+mysql_select_db($database_promusic, $promusic); //xxxjng
 
 $wwwhost = "http://localhost/promusic";
 ?>
